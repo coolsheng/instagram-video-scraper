24db1c72251a73c4fe4395b7806af17ea096785f
sheng <bahtmannn@gmail.com>
2025-05-28 08:24:10 +1000

    feat: removed logging

--- src/lib/utils.js
+++ src/lib/utils.js
@@ -44,8 +44,6 @@ const getVideoUrl = (html) => {
     throw new CustomError("Something went wrong, please try again", 500);
   }
 
-  console.log(`${$("video")}`);
-
   // Get video metadata
   const videoUrl = $("video").attr("src");
   if (!videoUrl) {
